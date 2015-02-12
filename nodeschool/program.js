09bb9832a81cb47b10008a4026f1cc456ccfba57
Chang Liu <shampliu@gmail.com>
2015-02-12 01:33:35 -0800

    folder setup

---
+++ nodeschool/program.js
@@ -0,0 +1,13 @@
+/* console.log("HELLO WORLD"); */
+
+// var sum=0
+// for (var i=2; i<process.argv.length; i++) {
+// 	sum+=Number(process.argv[i]);
+
+// }
+// console.log(sum);
+
+// var fs = require('fs');
+// var buff = fs.readFileSync(process.argv[2]).toString();
+// console.log(buff.split('\n').length - 1);
+
