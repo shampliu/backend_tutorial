f331462bf7b79d1b925a0f234228897ad59b83bc
Chang Liu <shampliu@gmail.com>
2015-02-18 21:37:23 -0800

    pre-delete

--- nodetest2/public/javascripts/global.js
+++ nodetest2/public/javascripts/global.js
@@ -1,11 +1,18 @@
 // Userlist data array for filling in info box
-var userListData = [];
+var userListData = []; // global variable if var is initialized in global scope
 
 // DOM Ready =============================================================
 $(document).ready(function() {
 
   // Populate the user table on initial page load
   populateTable();
+  
+  // Username link click
+  $('#userList table tbody').on('click', 'td a.linkshowuser', showUserInfo);
+
+  // Add User button click
+  $('#btnAddUser').on('click', addUser);
+
 
 });
 
@@ -21,7 +28,11 @@ function populateTable() {
   $.getJSON( '/users/userlist', function( data ) {
 
     // For each item in our JSON, add a table row and cells to the content string
-    $.each(data, function(){
+    $.each(data, function(){  
+
+      // Stick our user data array into a userlist variable in the global object
+      userListData = data;
+
       tableContent += '<tr>';
       tableContent += '<td><a href="#" class="linkshowuser" rel="' + this.username + '">' + this.username + '</a></td>';
       tableContent += '<td>' + this.email + '</td>';
@@ -33,3 +44,85 @@ function populateTable() {
     $('#userList table tbody').html(tableContent);
   });
 };
+
+// Show User Info
+function showUserInfo(event) {
+
+  // Prevent Link from Firing
+  event.preventDefault();
+
+  // Retrieve username from link rel attribute
+  var thisUserName = $(this).attr('rel');
+
+  // Get Index of object based on id value
+  var arrayPosition = userListData.map(function(arrayItem) { return arrayItem.username; }).indexOf(thisUserName);
+
+  // Get our User Object
+  var thisUserObject = userListData[arrayPosition];
+
+  //Populate Info Box
+  $('#userInfoName').text(thisUserObject.fullname);
+  $('#userInfoAge').text(thisUserObject.age);
+  $('#userInfoGender').text(thisUserObject.gender);
+  $('#userInfoLocation').text(thisUserObject.location);
+
+};
+
+// Add User
+function addUser(event) {
+    event.preventDefault();
+
+    // Super basic validation - increase errorCount variable if any fields are blank
+    var errorCount = 0;
+    $('#addUser input').each(function(index, val) {
+        if($(this).val() === '') { errorCount++; }
+    });
+
+    // Check and make sure errorCount's still at zero
+    if(errorCount === 0) {
+
+        // If it is, compile all user info into one object
+        var newUser = {
+            'username': $('#addUser fieldset input#inputUserName').val(),
+            'email': $('#addUser fieldset input#inputUserEmail').val(),
+            'fullname': $('#addUser fieldset input#inputUserFullname').val(),
+            'age': $('#addUser fieldset input#inputUserAge').val(),
+            'location': $('#addUser fieldset input#inputUserLocation').val(),
+            'gender': $('#addUser fieldset input#inputUserGender').val()
+        }
+
+        // Use AJAX to post the object to our adduser service
+        $.ajax({
+            type: 'POST',
+            data: newUser,
+            url: '/users/adduser',
+            dataType: 'JSON'
+        }).done(function( response ) {
+
+            // Check for successful (blank) response
+            if (response.msg === '') {
+
+                // Clear the form inputs
+                $('#addUser fieldset input').val('');
+
+                // Update the table
+                populateTable();
+
+            }
+            else {
+
+                // If something goes wrong, alert the error message that our service returned
+                alert('Error: ' + response.msg);
+
+            }
+        });
+    }
+    else {
+        // If errorCount is more than 0, error out
+        alert('Please fill in all fields');
+        return false;
+    }
+};
+
+
+
