c21ef1dbe8298315a0fc8bd54d5ed51bce0b96a9
Chang Liu <shampliu@gmail.com>
2015-02-24 02:27:47 -0800

    finished pt. 2

--- nodetest2/routes/users.js
+++ nodetest2/routes/users.js
@@ -21,5 +21,15 @@ router.post('/adduser', function(req, res) {
     });
 });
 
+/*
+ * DELETE to deleteuser.
+ */
+router.delete('/deleteuser/:id', function(req, res) {
+    var db = req.db;
+    var userToDelete = req.params.id;
+    db.collection('userlist').removeById(userToDelete, function(err, result) {
+        res.send((result === 1) ? { msg: '' } : { msg:'error: ' + err });
+    });
+});
 
 module.exports = router;
