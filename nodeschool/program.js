2ebd49a8b8e084e0663823d30d356f4573d5fe47
Chang Liu <shampliu@gmail.com>
2015-02-27 11:25:21 -0800

    My First Async I/O

--- nodeschool/program.js
+++ nodeschool/program.js
@@ -11,3 +11,13 @@
 // var buff = fs.readFileSync(process.argv[2]).toString();
 // console.log(buff.split('\n').length - 1);
 
+var fs = require('fs');
+var path = process.argv[2];
+// first two args are node and /path/to/program.js
+
+fs.readFile(path, 'utf8', function(err,data) {
+  var lines = data.split('\n');
+  console.log(lines.length-1);
+});
+
+
