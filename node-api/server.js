c38fbe792c316a4a38dd0a0942962f7b1789a253
Chang Liu <shampliu@gmail.com>
2015-03-05 00:39:24 -0800

    GET request for bear with name not working

--- node-api/server.js
+++ node-api/server.js
@@ -55,6 +55,34 @@ router.route('/bears')
 		})
 	})
 
+router.route('/bears/:bear_id')
+
+	// get the bear with that id
+	.get(function(req, res) {
+		Bear.findById(req.params.bear_id, function(err, bear) {
+			if (err)
+				res.send(err);
+			res.json(bear);
+		});
+	})
+
+	// update the bear with this id
+	.put(function(req, res) {
+		Bear.findById(req.params.bear_id, function(err, bear) {
+
+			if (err)
+				res.send(err);
+
+			bear.name = req.body.name;
+			bear.save(function(err) {
+				if (err)
+					res.send(err);
+
+				res.json({ message: 'Bear updated!' });
+			});
+
+		});
+	})
 
 
 // REGISTER OUR ROUTES -------------------------------
